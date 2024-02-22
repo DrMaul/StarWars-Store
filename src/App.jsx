75bb24a031abd676cc687574c9d7d9aba27ca273
DrMaul <agusfmartinez99@gmail.com>
2024-02-22 17:57:27 -0300

    agregando footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ItemDetailContainer from './components/ItemDetailContainer'
 import Error404 from './components/Error404'
 import Cart from './components/Cart'
 import CartContextProvider from './components/context/CartContext'
+import Footer from './components/Footer'
 import {Checkout}  from './components/Checkout'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -33,6 +34,8 @@ const App = () => {
 
       
         </Routes>
+
+        <Footer/>
 
       </BrowserRouter>
 
